4aaf15b3fd3714ca01230f86b8ceabf016c43130
Katherine Walendzik <katherinewalendzik@gmail.com>
2019-10-07 03:29:35 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,23 +14,7 @@ class Transfer
     @sender.valid? && @receiver.valid?
   end
   
-  
-   # let(:avi) { BankAccount.new("Avi") }
-   # let(:amanda) { BankAccount.new("Amanda") }
-   # let(:transfer) { Transfer.new(amanda, avi, 50) }
-   # let(:bad_transfer) { Transfer.new(amanda, avi, 4000) }
-
-   # it "can execute a successful transaction between two accounts" do
-   #   transfer.execute_transaction
-   #  expect(amanda.balance).to eq(950)
-   #  expect(avi.balance).to eq(1050)
-   #   expect(transfer.status).to eq("complete")
-  
   def execute_transaction
-    #execute successful transaction between 2 accts
-    #each transfer can only happen once
-    #rejects a transfer if the sender does not have enough funds (does not have a valid account)
-    
     if valid? && sender.balance > amount && self.status == "pending"
       receiver.balance += amount
       sender.balance -= amount
