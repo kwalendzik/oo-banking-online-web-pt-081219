8ed1d9569d15be7ba4f0ed35fe351b0bfc02d680
Katherine Walendzik <katherinewalendzik@gmail.com>
2019-10-07 03:35:20 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -20,13 +20,23 @@ class Transfer
       sender.balance -= amount
       self.status = "complete"
     else
-      self.status = "rejected"
-      "Transaction rejected. Please check your account balance."
+      reject_transfer
     end
   end
   
   def reverse_transfer
-   
+    if valid? && receiver.balance > amount && self.status == "complete"
+      receiver.balance -= amount
+      sender.balance += amount
+      self.status = "reversed"
+    else
+      reject_transfer
+    end
+  end
+  
+  def reject_transfer
+    self.status = "rejected"
+    "Transaction rejected. Please check your account balance."
   end
 
 end
